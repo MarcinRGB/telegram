a783d24baf3ffd256837fdde7b0a2ed17ea8c6ff
MarcinRGB <marcin92tomczyk@gmail.com>
2026-01-19 05:47:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 import os
-from telegram import Bot
+import requests
 
-# pobieramy token i chat_id z GitHub Secrets
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-bot = Bot(token=TOKEN)
-bot.send_message(chat_id=CHAT_ID, text="✅ Bot działa!")
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+payload = {
+    "chat_id": CHAT_ID,
+    "text": "✅ Bot działa (test z GitHub Actions)"
+}
+
+response = requests.post(url, data=payload)
+print(response.text)
